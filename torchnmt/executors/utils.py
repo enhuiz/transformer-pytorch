14d684497a3eb9ac7d1c407d8567aec1f93d7fbc
enhuiz <niuzhe.nz@outlook.com>
2019-06-21 15:25:30 +0800

    update

--- torchnmt/executors/utils.py
+++ torchnmt/executors/utils.py
@@ -1,16 +1,6 @@
 import os
-import time
 import glob
-
-import tqdm
-import pandas as pd
 import torch
-from torch.utils.data import DataLoader
-from torch.utils.data.dataloader import default_collate
-from tensorboardX import SummaryWriter
-
-from torchnmt import networks, datasets
-from torchnmt.scores import compute_scores
 
 
 class CheckpointSaver(object):
@@ -42,40 +32,3 @@ class CheckpointSaver(object):
         except:
             pass
         return step
-
-
-class Executor(object):
-    def __init__(self, name, model, dataset, opts):
-        """
-        Args:
-            model: model opts
-            dataset: dataset opts
-        """
-        self.name = name
-        self.model = model
-        self.dataset = dataset
-        self.opts = opts
-
-        self.writer = SummaryWriter('runs/{}'.format(self.name))
-        self.saver = CheckpointSaver('ckpt/{}'.format(self.name))
-
-    def create_model(self, state_dict=None):
-        if state_dict is not None:
-            self.model.state_dict = state_dict
-        return networks.get(self.model).to(self.opts.device)
-
-    def create_dataset(self, split):
-        return datasets.get(self.dataset, split)
-
-    def create_data_loader(self, split, shuffle=False):
-        dataset = self.create_dataset(split)
-
-        if hasattr(dataset, 'get_collate_fn'):
-            collate_fn = dataset.get_collate_fn()
-        else:
-            collate_fn = default_collate
-
-        return DataLoader(dataset, self.opts.batch_size,
-                          shuffle=shuffle,
-                          num_workers=4,
-                          collate_fn=collate_fn)
